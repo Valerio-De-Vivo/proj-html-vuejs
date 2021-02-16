a759b4c89933c5e77f2118801a575a930fc1f40f
Valerio <valeriodevivo90@gmail.com>
2021-02-16 17:08:21 +0100

    finish slider img

--- js/script.js
+++ js/script.js
@@ -1,6 +1,24 @@
 var app = new Vue({
+    el: '#jumbo',
+    data:{
+        img: ['1.png', '2.png', '3.png'],
+        count:0
+    },
+    methods:{
+        slider(i) {
+            this.count = i;
+        }
+    }
+});
+
+var app = new Vue({
     el: '#fourj',
     data:{
-        gallery: [1,2,3,4,5,6,7,8]
-    },
+        gallery: [1,2,3,4,5,6,7,8],
+
+    }
 });
+
+
+
+
