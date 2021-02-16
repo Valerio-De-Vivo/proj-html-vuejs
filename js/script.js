d6bc4af2537293acdb9174ef03efa2ac4b844a47
Valerio <valeriodevivo90@gmail.com>
2021-02-16 17:28:09 +0100

    modal search bar done

--- js/script.js
+++ js/script.js
@@ -15,9 +15,24 @@ var app = new Vue({
     el: '#fourj',
     data:{
         gallery: [1,2,3,4,5,6,7,8],
-
     }
 });
+
+let iconSearch = document.querySelector('.sear');
+let barSearch = document.querySelector('.search-bar');
+let close = document.querySelector('.close');
+
+iconSearch.addEventListener("click", function() {
+    barSearch.classList.toggle('display');
+}); 
+
+close.addEventListener("click", function(){
+    barSearch.classList.toggle('display');
+});
+
+
+
+
 
 
 
