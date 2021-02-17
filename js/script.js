c40f03d6486065ac019e6f9aad32f61d2cad8a37
Valerio <valeriodevivo90@gmail.com>
2021-02-17 11:42:25 +0100

    Finish with animation and scroll to top

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ var app = new Vue({
         slider(i) {
             this.count = i;
             gsap.from(".jumbo-img img", {x:'100%'});
-        }
+        },
     }
 });
 
@@ -46,6 +46,74 @@ gsap.timeline()
     .from(".text-area a", {opacity:0, y:'1000px', stagger: .1})
     .from(".jumbo-img", {x:'120%', duration: .8, ease: "back.out"})
     .from(".num-slider", {scale:0, delay:0.5});
+
+gsap.from(".icons", {
+    scrollTrigger: {
+        trigger: ".icon",
+        start: "top bottom",
+        end:"center center",
+        toggleActions: "restart none none none ",
+        scrub:true
+    },
+    xPercent:400,
+    opacity: 0,
+});
+
+gsap.from(".third-img", {
+    scrollTrigger: {
+        trigger: ".third-text",
+        start: "top bottom",
+        end:"center center",
+        toggleActions: "restart none none none ",
+        scrub:true
+    },
+    xPercent:-400,
+    opacity: 0,
+});
+
+gsap.from(".row-gallery img", {
+    scrollTrigger: {
+        trigger: ".row-text",
+        start: "top top",
+        end:"center center"
+    },
+    scale:0,
+    duration:2,
+    stagger: .2,
+    ease:"bounce"
+});
+
+gsap.from(".blog-article", {
+    scrollTrigger: {
+        trigger: ".title",
+        start: "top 200px",
+        end:"bottom center"
+    },
+    xPercent:400,
+    duration:1.5,
+    stagger: .2
+});
+
+gsap.from(".client", {
+    scrollTrigger: {
+        trigger: ".talk",
+        start: "top 200px",
+        end:"bottom center"
+    },
+    xPercent:400,
+    duration:.6,
+    stagger: .2
+});
+
+gsap.from(".scroll-top", {
+    scrollTrigger: {
+        trigger: ".third-text",
+        start: "top 1200px",
+        scrub:true
+    },
+    opacity: 0,
+    duration:.6
+});
 
 
 
